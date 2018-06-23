341e689d21c8a78cfbb09cae93377b54117d6e67
pskindero <skinderowicz.patryk@gmail.com>
2018-06-23 11:40:00 +0200

    System resintall

--- DesignPatterns/src/main/java/com/pskindero/learning/designpatterns/strategy/MainClass.java
+++ DesignPatterns/src/main/java/com/pskindero/learning/designpatterns/strategy/MainClass.java
@@ -1,10 +1,18 @@
 package com.pskindero.learning.designpatterns.strategy;
 
+/**
+ * STRATEGY
+ * - behaviour
+ * 
+ * We create various strategy objects and a context objects whose behaviour veries
+ * as per its strategy object. The strategy object changes the executing algorithm
+ * of the context object. 
+ *  
+ */
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 	}
 
 }
---
+++ java/Spring/SpringData/src/main/java/com/pskindero/learning/spring/data/repositories/Example.java
@@ -0,0 +1,8 @@
+package com.pskindero.learning.spring.data.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Example implements CrudRepository{
+
+}
